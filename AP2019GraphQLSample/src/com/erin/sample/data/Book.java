d9d2ba3620cfa609337f33dc0bb71b01d952fba8
Ya Peng Dong <dypdong@cn.ibm.com>
2019-07-22 15:00:43 +0800

    APtest

--- AP2019GraphQLSample/src/com/erin/sample/data/Book.java
+++ AP2019GraphQLSample/src/com/erin/sample/data/Book.java
@@ -58,7 +58,7 @@ public class Book {
 					{	
 						bookData = new BookData(bdco.get());
 						return  ImmutableMap.of("id", bookData.getBookId(),
-			                    "name", "AP_"+bookData.getBookName(),
+			                    "name", "APtest_"+bookData.getBookName(),
 			                    "pageCount", bookData.getPagecount(),
 			                    "authorId", bookData.getAuthrId());
 					}
